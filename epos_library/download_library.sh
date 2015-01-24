ce49573f71c249fa1b9fa26d408e761d7c5a1cab
Mitchell Wills <mwills@wpi.edu>
2015-01-23 23:33:55 -0500

    Added libraries back to repository instead of downloading on build

--- epos_library/download_library.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -f EPOS-Linux-Library-En.zip ]; then wget "http://www.maxonmotorusa.com/medias/sys_master/root/8815100330014/EPOS-Linux-Library-En.zip";fi
-if [ ! -d EPOS_Linux_Library ]; then unzip "EPOS-Linux-Library-En.zip";fi
